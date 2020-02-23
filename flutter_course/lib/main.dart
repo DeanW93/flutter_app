74cc3d2cece2b995247d583fd662c7053414f975
DeanW93 <deanwhelanhsd@hotmail.com>
2020-02-23 20:22:37 +0000

    fixes

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,15 +1,1 @@
 
-import 'package:flutter/material.dart';
-
-main() {
-
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(home: ,);
-  }
-
-}
